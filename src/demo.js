18c17eb46b2b47d9d43e8d080b139ccc6fbb339e
Akshay Rajkumar <akshayrajkumar96@gmail.com>
2020-01-20 16:25:48 +0530

    Initial commit

---
+++ src/demo.js
@@ -0,0 +1,57 @@
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import BottomNavigation from "@material-ui/core/BottomNavigation";
+import BottomNavigationAction from "@material-ui/core/BottomNavigationAction";
+import HomeOutlinedIcon from "@material-ui/icons/HomeOutlined";
+import NotificationsNoneOutlinedIcon from "@material-ui/icons/NotificationsNoneOutlined";
+import AddCircleOutlineOutlinedIcon from "@material-ui/icons/AddCircleOutlineOutlined";
+import DashboardOutlinedIcon from "@material-ui/icons/DashboardOutlined";
+import ExploreOutlinedIcon from "@material-ui/icons/ExploreOutlined";
+const useStyles = makeStyles({
+  root: {
+    width: 500
+  }
+});
+
+export default function LabelBottomNavigation() {
+  const classes = useStyles();
+  const [value, setValue] = React.useState("recents");
+
+  const handleChange = (event, newValue) => {
+    setValue(newValue);
+  };
+
+  return (
+    <BottomNavigation
+      value={value}
+      onChange={handleChange}
+      className={classes.root}
+    >
+      <BottomNavigationAction
+        label=""
+        value="Recents"
+        icon={<HomeOutlinedIcon />}
+      />
+      <BottomNavigationAction
+        label=""
+        value="Explore"
+        icon={<ExploreOutlinedIcon />}
+      />
+      <BottomNavigationAction
+        label=""
+        value="Post"
+        icon={<AddCircleOutlineOutlinedIcon />}
+      />
+      <BottomNavigationAction
+        label=""
+        value="Notification"
+        icon={<NotificationsNoneOutlinedIcon />}
+      />
+      <BottomNavigationAction
+        label=""
+        value="Profile"
+        icon={<DashboardOutlinedIcon />}
+      />
+    </BottomNavigation>
+  );
+}
